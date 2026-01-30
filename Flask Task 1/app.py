d3d220cf82a6cc77f683fa61e0a936e17cb10253
Sharwani Bale <164863542+Sharwani987@users.noreply.github.com>
2026-01-30 22:19:47 +0530

    Add files via upload

--- Flask Task 1/app.py
+++ Flask Task 1/app.py
@@ -1,1 +1,25 @@
+from flask import Flask, request
+from datetime import datetime
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    name = request.args.get('name', '')
+    upper_name = name.upper()
+
+    # Get current hour
+    hour = datetime.now().hour
+
+    # Decide greeting
+    if hour < 12:
+        greeting = "Good Morning"
+    elif hour < 17:
+        greeting = "Good Afternoon"
+    else:
+        greeting = "Good Evening"
+
+    return f"<h1>{greeting}, {upper_name}!</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
